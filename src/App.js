434891e0e83ca095148ad21ef6304502cb0d60e7
Juan Manuel Fernandez Daza <tachitofer@hotmail.com>
2020-02-02 16:54:56 -0500

    Here I organized some of the code in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React</a>
       </header>
     </div>
   );
